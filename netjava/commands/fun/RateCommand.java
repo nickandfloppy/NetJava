61258d122708d0d7cfa28cb66d4390cbc595e529
Frankie B <floppydisk05@aol.com>
2022-03-23 01:09:21 +0000

    Added r i g g i n g

--- netjava/commands/fun/RateCommand.java
+++ netjava/commands/fun/RateCommand.java
@@ -18,11 +18,22 @@ public class RateCommand implements MessageCreateListener {
 		if (event.getMessageContent().startsWith((Bot.config.prefix + cmd.command).toLowerCase())) {
             String item = event.getMessageContent().toLowerCase().replaceFirst(Bot.config.prefix + cmd.command + " ", "");
             String score;
+            EmbedBuilder embed = new EmbedBuilder();
             if (item.toLowerCase().equals("saab")) { score = "GRIMP"; }
+            else if (item.toLowerCase().equals("boobs")) { score = "11"; }
+            else if (item.toLowerCase().equals("hitler")) { score = "-1"; }
+            else if (item.toLowerCase().equals("putin")) {
+                score = "SUCC";
+                embed.setImage("https://c.tenor.com/KjUtiyx4GhwAAAAC/wide-vladimir-putin.gif");
+            }
+            else if (item.toLowerCase().equals("boris") || item.toLowerCase().equals("bojo")) {
+                score = "borbis";
+                embed.setImage("https://c.tenor.com/678K-ndXHuIAAAAd/boris-zip-line.gif");
+                embed.setFooter("Powered by borbisphone. Now with borbis callig integrated");
+            }
 			else { score = Integer.toString((int)(Math.random() * 11)); }
-			EmbedBuilder embed = new EmbedBuilder()
-                .setTitle("🤔 I'd give " + item + " a solid " + score + "/10")
-                .setColor(new Color(50, 106, 201));
+            embed.setTitle("🤔 I'd give " + item + " a solid " + score + "/10");
+            embed.setColor(new Color(50, 106, 201));
 			event.getChannel().sendMessage(embed)
 				.exceptionally(ExceptionLogger.get(MissingPermissionsException.class));
 		}
